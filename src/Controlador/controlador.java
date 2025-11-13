32dfe801e94e9fc9c118a767832be8c597dbb8a7
citrix <melissaruizdelg21@gmail.com>
2025-11-13 13:08:58 -0500

    distancia levenshtein en controlador

--- src/Controlador/controlador.java
+++ src/Controlador/controlador.java
@@ -57,8 +57,38 @@ public class controlador {
         }
     }
     
-    public static void main(String[] args) {
+    public static int DistanciaLevenshtein (String cad1, String cad2){
+        int[][] distancia = new int [cad1.length()+1][cad2.length()+1]; //+1 paraobtener los datos iniciales
+        
+        //  columna - i eliminaciones
+        for (int i = 0; i <= cad1.length(); i++) { //recorrer todo cad1
+            distancia[i][0] = i; 
+        }
+        // fila - j inserciones
+        for (int j = 0; j <= cad2.length(); j++) { 
+            distancia[0][j] = j; 
+        }
+        //rellenar distancia[][]
+        for (int i = 1; i <= cad1.length(); i++) {
+            for (int j = 1; j <= cad2.length(); j++) {
+                if(cad1.charAt(i-1) == cad2.charAt(j-1)){
+                    distancia[i][j] = distancia[i-1][j-1]; //Coinciden ... toma el valor de la diagonal superior izquierda
+                    
+                }else{
+                    int eliminar = distancia[i-1][j]; // dato superior
+                    int insertar = distancia[i][j-1]; // dato de la izquierda
+                    int sustituir = distancia[i-1][j-1]; // dato diagonal superior izquierda
+                    
+                    distancia[i][j] = 1 + Math.min(eliminar, Math.min(insertar, sustituir)); //elige el menor valor
+                }   
+            }
+        }
+        return distancia[cad1.length()][cad2.length()]; //numero de cambios = ultima celda de la matriz
+    }
     
+    public static void main(String[] args) {
+        //prueba de levenshtein
+        System.out.println(DistanciaLevenshtein("gato","cata"));
     }
     
 }
